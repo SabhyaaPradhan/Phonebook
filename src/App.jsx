824ba29f13423acb4c1d358407a27e55367074aa
unknown <sabhyaapradhan2005@gmail.com>
2023-12-28 17:51:40 +0545

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,56 @@
-import React, { useState } from "react";
-import Note from "./Note"
+import React, { useState, useEffect } from "react";
+import noteService from './services/notes';
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Sabhyaa', number: '9054342312' }]);
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchQuery, setSearchQuery] = useState('')
-  const [error, setError] = useState('')
-  
 
   const addPerson = (event) => {
     event.preventDefault()
 
-    const nameExists = persons.some(person => person.name === newName)
+    const nameExists = persons.some(person => person.name === newName);
 
     if (nameExists) {
       const existingPerson = persons.find(person => person.name === newName);
-      window.alert(`${existingPerson.name} already exists in the phonebook with number`);
-    }
-    else {
+      window.alert(`${existingPerson.name} already exists in the phonebook with ${existingPerson.number}`);
+    } else {
       const newPerson = { name: newName, number: newNumber }
-      setPersons([...persons, newPerson])
-      setNewName('')
-      setNewNumber('')
-      setError('')
+
+      noteService.create(newPerson)
+        .then(response => {
+          setPersons([...persons, response.data])
+          setNewName('')
+          setNewNumber('')
+        })
+        .catch(error => {
+          console.error('Error adding person:', error);
+        })
     }
   }
 
-  const filteredPersons = persons.filter(person =>
-    person.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
-    person.number.includes(searchQuery)
-  )
+  const deletePerson = (id, name) => {
+    const confirmed = window.confirm(`Delete ${name} from the phonebook?`);
+    if (confirmed) {
+      noteService
+      .deletePerson(id)
+      .then(() => {
+        setPersons(persons.filter(person => person.id !==id))
+      })
+      .catch(error => {
+        console.error('Error deleting person:', error);
+      })
+    }
+  }
+
+  useEffect(() => {
+    noteService
+    .getAll()
+    .then(response => {
+      setPersons(response.data)
+    })
+  }, [])
 
   return (
     <div>
@@ -64,8 +84,10 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {filteredPersons.map((person, index) => (
-          <Note key={index} person={person} />
+        {persons.map((person, index) => (
+          <li key={index}>{person.name}: {person.number}
+          <button onClick={() => deletePerson(person.id, person.name)}>Delete</button>
+          </li>
         ))}
       </ul>
     </div>
